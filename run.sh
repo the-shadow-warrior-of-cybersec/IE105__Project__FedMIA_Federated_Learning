90bae16db9d41af3af778facddc007ac768cbc47
vobao-xD <vobao8682@gmail.com>
2025-05-17 05:33:11 +0000

    Update UI

--- run.sh
+++ run.sh
@@ -1,26 +1,49 @@
-# federated training and attack measurement calculatinging command
-dataset=cifar100 # or dermnet
-model_name=alexnet # or ResNet18
-
+# Thiết lập các siêu tham số và cấu hình
+dataset=cifar100 # cifar100 hoặc dermnet
+model_name=alexnet # alexnet hoặc ResNet18
 opt=sgd
 seed=1
 lr=0.1
 local_epoch=1
 
-# iid experiment
-save_dir=log_fedmia/iid
-mkdir -p "$save_dir"
-CUDA_VISIBLE_DEVICES=1 python main.py --seed $seed --num_users 10 --iid 1   \
- --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
- --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
- --lr_up cosine --MIA_mode 1  --gpu 1 &> "${save_dir}/raw_logs" &
+# In menu
+clear
+echo "#################### Chọn chế độ thử nghiệm (cách chia dataset cho các client) ####################"
+echo
+echo "1. Chế độ IID (dữ liệu ở các client có nhiều tương quan)"
+echo
+echo "2. Chế độ non-IID (dữ liệu ở các client phân biệt)" 
+echo
+read -p "---> Nhập lựa chọn của bạn (1/2): " choice
+echo
 
-# non-iid experiment
-save_dir=log_fedmia/noniid
-mkdir -p "$save_dir"
-CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --num_users 10 --iid 0 --beta 1.0  \
- --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
- --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
- --lr_up cosine --MIA_mode 1  --gpu 0 2>&1 | tee "${save_dir}/raw_logs" &
 
-wait
+
+# Logic chính
+if [ "$choice" = "1" ]; then
+    # iid experiment
+    save_dir=log_fedmia/iid
+    mkdir -p "$save_dir"
+    echo "#################### Thử nghiệm IID ####################"
+    echo
+    CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --num_users 10 --iid 1 \
+     --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
+     --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
+     --lr_up cosine --MIA_mode 1 --gpu 0 2>&1 | tee "${save_dir}/raw_logs"
+elif [ "$mode_choice" = "2" ]; then
+    # non-iid experiment
+    save_dir=log_fedmia/noniid
+    mkdir -p "$save_dir"
+    echo "#################### Thử nghiệm Non-IID ####################"
+    echo
+    CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --num_users 10 --iid 0 --beta 1.0 \
+     --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
+     --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
+     --lr_up cosine --MIA_mode 1 --gpu 0 2>&1 | tee "${save_dir}/raw_logs"
+else
+    echo "Invalid choice. Please run the script again and select 1 or 2."
+    exit 1
+fi
+
+echo
+echo "#################### Hoàn thành thử nghiệm ####################"
